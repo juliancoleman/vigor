26ab5710522999cb633423fdc90e87b7c6fa5e84
juliancoleman <julcol03@gmail.com>
2016-04-25 02:10:23 -0700

    got some major work done using the Django docs

--- vigor_app/settings.py
+++ vigor_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vigor.apps.VigorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
