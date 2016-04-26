e936567f243a3f83f6670481eb134331c2a660e3
juliancoleman <julcol03@gmail.com>
2016-04-26 01:37:57 -0700

    added some routes

--- vigor/views.py
+++ vigor/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Vigor index.")
+
+def login(request):
+    return HttpResponse("Login Page.")
+
+def signup(request):
+    return HttpResponse("Signup Page.")
+
+def dashboard(request):
+    return HttpResponse("Dashboard Page.")
+
+def profile(request):
+    return HttpResponse("Profile Page.")
