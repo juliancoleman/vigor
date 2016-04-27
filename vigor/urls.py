0a4a96d52a0354c972b8c3849ec27976a6c1ce30
juliancoleman <julcol03@gmail.com>
2016-04-27 15:53:41 -0700

    finished signup feature

--- vigor/urls.py
+++ vigor/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "vigor"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login', views.login, name='login'),
     url(r'^signup', views.signup, name='signup'),
     url(r'^dashboard', views.dashboard, name='dashboard'),
     url(r'^profile', views.profile, name='profile'),
+    url(r'^add_item', views.add_item, name='add_item')
 ]
