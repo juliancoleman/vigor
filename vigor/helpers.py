89413484a282ef0676918a3ef644cfbb0dbf17d2
juliancoleman <julcol03@gmail.com>
2016-05-05 17:26:13 -0700

    reworked invalid syntax

--- vigor/helpers.py
+++ vigor/helpers.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def is_authenticated(request):
-    return HttpResponseRedirect("/vigor/dashboard") if request.user.is_authenticated()
-end
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/vigor/dashboard")
