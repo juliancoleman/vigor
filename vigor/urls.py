6f2848172133157d8026d4b3ef8bf228299a5e4a
juliancoleman <julcol03@gmail.com>
2016-05-05 20:40:43 -0700

    looks like we got logout working

--- vigor/urls.py
+++ vigor/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', views.logout, name='logout'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^add_item/$', views.add_item, name='add_item'),
