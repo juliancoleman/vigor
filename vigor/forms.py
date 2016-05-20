dfe23515f9e1715e3c9ccc07ca9e4fa192cc03eb
juliancoleman <julcol03@gmail.com>
2016-05-20 13:15:24 -0700

    got rid of unnecessary commented code

--- vigor/forms.py
+++ vigor/forms.py
@@ -39,19 +39,3 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = UserPrefs
         fields = ('gender', 'birth_year',)
-# from django.contrib.auth.forms import UserCreationForm
-
-# class UserCreationFormExtended(UserCreationForm):
-#     def __init__(self, *args, **kwargs):
-#         super(UserCreationFormExtended, self).__init__(*args, **kwargs)
-#         self.fields['email'].required = True
-#
-#     def clean_email(self):
-#         email = self.cleaned_data.get('email')
-#         if email and User.objects.filter(email=email).count():
-#             raise forms.ValidationError('This email is already bound to an existing profile')
-#         return email
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email')
--- vigor/urls.py
+++ vigor/urls.py
@@ -9,10 +9,4 @@ urlpatterns = [
     url(r'^register/$', 'vigor.ajax.register', name='register'),
     url(r'^preferencess/$', 'vigor.views.preferences', name='preferences'),
     url(r'^dashboard/$', 'vigor.views.dashboard', name='dashboard')
-    # url(r'^auth/$', views.signin, name='login'),
-    # url(r'^logout/$', views.logout, name='logout'),
-    # url(r'^dashboard/$', views.dashboard, name='dashboard'),
-    # url(r'^profile/$', views.profile, name='profile'),
-    # url(r'^add_item/$', views.add_item, name='add_item'),
-    # url(r'^users/$', views.users, name='users')
 ]
