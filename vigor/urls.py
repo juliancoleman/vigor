20f1a5a68c31fb58a44b304dd97b11808b6d23a6
juliancoleman <julcol03@gmail.com>
2016-05-21 12:05:09 -0700

    must pass the callable as of Django 1.10; migrate to reflect this

--- vigor/urls.py
+++ vigor/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from . import ajax
 
 urlpatterns = [
-    url(r'^$', 'vigor.views.index', name='index'),
-    url(r'^sign_out/$', 'vigor.ajax.sign_out', name='sign_out'),
-    url(r'^signin/$', 'vigor.views.login_view', name='login'),
-    url(r'^register/$', 'vigor.ajax.register', name='register'),
-    url(r'^preferences/$', 'vigor.views.preferences', name='preferences'),
-    url(r'^dashboard/$', 'vigor.views.dashboard', name='dashboard')
+    url(r'^$', views.index, name='index'),
+    url(r'^sign_out/$', ajax.sign_out, name='sign_out'),
+    url(r'^signin/$', views.login_view, name='login'),
+    url(r'^register/$', ajax.register, name='register'),
+    url(r'^preferences/$', views.preferences, name='preferences'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard')
 ]
