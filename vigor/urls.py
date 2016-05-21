bb771d0cdd7c0f6d995d5cdbceb8425710a1dabc
juliancoleman <julcol03@gmail.com>
2016-05-21 12:00:01 -0700

    fixed typo

--- vigor/urls.py
+++ vigor/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^sign_out/$', 'vigor.ajax.sign_out', name='sign_out'),
     url(r'^signin/$', 'vigor.views.login_view', name='login'),
     url(r'^register/$', 'vigor.ajax.register', name='register'),
-    url(r'^preferencess/$', 'vigor.views.preferences', name='preferences'),
+    url(r'^preferences/$', 'vigor.views.preferences', name='preferences'),
     url(r'^dashboard/$', 'vigor.views.dashboard', name='dashboard')
 ]
