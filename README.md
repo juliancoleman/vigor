d298157a19930bbc72981d77fd103bd381a8ce10
juliancoleman <julcol03@gmail.com>
2016-05-24 18:50:19 -0700

    dump

--- README.md
+++
@@ -1,2 +0,0 @@
-[![Stories in Ready](https://badge.waffle.io/juliancoleman/vigor-app.png?label=ready&title=Ready)](https://waffle.io/juliancoleman/vigor-app)
-# vigor-app
--- vigor/helpers.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def redirect_if_authenticated(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect("/vigor/dashboard")
